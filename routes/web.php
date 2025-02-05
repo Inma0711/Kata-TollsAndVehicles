435ca975c223965a27e21cf25a98826b070540a9
Inma Gonzalez <inmagonzalez045@gmail.com>
2025-02-05 15:17:31 +0100

    feat: add web routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/vehicles', [VehicleController::class, 'index'])->name('vehicles.home');
+Route::get('/vehicles/{id}', [VehicleController::class, 'show'])->name('vehicles.show');
+Route::get('/stations', [StationController::class, 'index'])->name('stations.home');
+Route::get('/stations/{id}', [StationController::class, 'show'])->name('stations.show');
+
