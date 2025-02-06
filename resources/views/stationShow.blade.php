5cf7a3f1721f1cc8cea24e4f5c17eb3f3a38cad9
Inma Gonzalez <inmagonzalez045@gmail.com>
2025-02-06 10:52:50 +0100

    fix/ add style station show

--- resources/views/stationShow.blade.php
+++ resources/views/stationShow.blade.php
@@ -1,43 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="containerMain">
-    <div class="containerTitle"> 
-        <div class="stationAtribute">
-            <p class="title">Nombre:</p>
-            <p>{{ $station->name }}</p>
-        </div>
-        <div class="stationAtribute">
-            <p class="title">Lugar:</p>
-            <p>{{ $station->city }}</p>
-        </div>
-        <div class="stationAtribute">
-            <p class="title">Recolectado:</p>
-            <p>{{ $station->total_toll_collected }}</p>
-        </div>
-    </div>
-    <div class="objectList">
-        <h1>VEHÍCULOS PASADOS</h1>
-    </div>
-
-
-    <div class="cardSection container">
-        @foreach ($station->tolls as $toll)
-           
-                <h5 class="card-title">{{ $toll->vehicle->plate }}</h5>
-                       <!-- <p>{{ ucfirst($toll->vehicle->type->type) }}</p>-->
-                    </div>
-                    <p><strong>Num of axles:</strong> {{ $toll->vehicle->axles ? $toll->vehicle->axles : 0 }}</p>
-                    <p><strong>Amount paid:</strong> {{ $toll->toll_value }}€</p>
-                </div>
-                <a href="{{ route('vehiclesShow', $toll->vehicle->id) }}" class="btn btn-details">Details</a>
-                </div>
+    <div class="containerMain">
+        <div class="containerTitle">
+            <div class="stationAtribute">
+                <p class="title">Nombre:</p>
+                <p>{{ $station->name }}</p>
             </div>
+            <div class="stationAtribute">
+                <p class="title">Lugar:</p>
+                <p>{{ $station->city }}</p>
+            </div>
+            <div class="stationAtribute">
+                <p class="title">Recolectado:</p>
+                <p>{{ $station->total_toll_collected }}</p>
+            </div>
+        </div>
+        <div class="objectList">
+            <h1>VEHÍCULOS PASADOS</h1>
+        </div>
+
+
+       <div class="objectList">
+            @foreach ($station->tolls as $toll)
+            <div class="item">
+                <p><strong>Vehículo:</strong>{{ ucfirst($toll->vehicle->type->type) }}</p>
+
+                <p><strong>Número de ejes:</strong> {{ $toll->vehicle->axles ? $toll->vehicle->axles : 0 }}</p>
+                <p><strong>Dinero a pagar:</strong> {{ $toll->toll_value }}€</p>
+
+                <a href="{{ route('vehicleShow', $toll->vehicle->id) }}" class="detailsButton">Detalles</a>
+        </div>
         @endforeach
+
     </div>
-</div>
-
-</div>
-
-
 @endsection
+</div>
